e213f4b002154d876528c9ad9b908170cf0036ce
letrungnhan <19130157@st.hcmuaf.edu.vn>
2022-07-12 18:37:23 +0700

    init

---
+++ Ictshop/Controllers/UserController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+namespace Ictshop.Controllers
+{
+    public class UserController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        // ĐĂNG KÝ
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+
+        // ĐĂNG KÝ PHƯƠNG THỨC POST
+        [HttpPost]
+        public ActionResult Dangky(Nguoidung nguoidung)
+        {
+            try
+            {
+                // Thêm người dùng  mới
+                db.Nguoidungs.Add(nguoidung);
+                // Lưu lại vào cơ sở dữ liệu
+                db.SaveChanges();
+                // Nếu dữ liệu đúng thì trả về trang đăng nhập
+                if (ModelState.IsValid)
+                    {
+                        return RedirectToAction("Dangnhap");
+                    }
+                return View("Dangky");
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+   
+        public ActionResult Dangnhap()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(FormCollection userForm)
+        {
+            if (ModelState.IsValid)
+            {
+
+                string email = userForm["TaiKhoan"].ToString();
+                string password = userForm["MatKhau"].ToString();
+
+
+                var f_password = GetMD5(password);
+                var data = db.Nguoidungs.Where(s => s.Email.Equals(email) && s.Matkhau.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().Hoten;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idUser"] = data.FirstOrDefault().MaNguoiDung;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+
+        }
+
+        private object GetMD5(string str)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            byte[] bHash = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+
+            StringBuilder sbHash = new StringBuilder();
+
+            foreach (byte b in bHash)
+            {
+
+                sbHash.Append(String.Format("{0:x2}", b));
+
+            }
+
+            return sbHash.ToString();
+        }
+
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection userlog)
+        {
+            string userMail = userlog["userMail"].ToString();
+            string password = userlog["password"].ToString();
+            var islogin = db.Nguoidungs.SingleOrDefault(x => x.Email.Equals(userMail) && x.Matkhau.Equals(password));
+
+            if (islogin != null)
+                {
+                    if (userMail == "Admin@gmail.com")
+                        {
+                           Session["use"] = islogin;
+                           return RedirectToAction("Index", "Admin/Home");
+                        }
+                     else
+                         {
+                           Session["use"] = islogin;
+                           return RedirectToAction("Index","Home");
+                         }
+                 }
+            else
+                {
+                    ViewBag.Fail = "Đăng nhập thất bại";
+                    return View("Dangnhap");
+                }
+
+        }
+        public ActionResult DangXuat()
+        {
+            Session["use"] = null;
+            return RedirectToAction("Index","Home");
+
+        }
+
+
+    }
+}
